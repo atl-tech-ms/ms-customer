3cc0bbc6cb62ed5f1426af579b3ab9cbf450c14f
thehasanovv <hasanov.e95@gmail.com>
2024-09-16 15:47:16 +0400

    Fixed bugs

--- src/main/java/az/atl/customerms/model/request/SaveCustomerDto.java
+++ src/main/java/az/atl/customerms/model/request/SaveCustomerDto.java
@@ -1,7 +1,6 @@
 package az.atl.customerms.model.request;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -21,10 +20,13 @@ public class SaveCustomerDto {
 
     @Size(min = 3, max = 50, message = "Name Required - The name must be between 3 and 50 characters.")
     String fullName;
+
     @NotNull(message = "Age Required")
     Integer age;
+
     @NotBlank(message = "Pin Required")
     String pin;
+
     @NotNull(message = "Balance Required")
     BigDecimal balance;
 }
